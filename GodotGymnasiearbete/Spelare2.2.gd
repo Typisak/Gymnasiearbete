fcb61b9ce9f0fdf05084b9212c5257c433fe032a
Typisak <115471902+Typisak@users.noreply.github.com>
2022-10-17 16:27:35 +0200

    Onödiga filer borttagna, Player script ändrat, bana utökad, meny påbörjad,

--- GodotGymnasiearbete/Spelare2.2.gd
+++ GodotGymnasiearbete/Spelare2.2.gd
@@ -1,40 +1,77 @@
 extends KinematicBody2D
 
-export (int) var run_speed = 300
-export (int) var jump_speed = -350
-export (int) var gravity = 800
+const UP_DIRECTION = Vector2.UP #Kan ändras för att gå på väggar etc.
 
-var velocity = Vector2()
-#var jumping = true
-#var walking = true
+export var speed = 250 #Karaktärens gånghastighet
 
-func get_input():
-	velocity.x = 0
+export var gravity = 600 #Gravitationens styrka
+export var jump_quantity = 1 #Antalet hopp karaktären kan göra
+export var jump_strength = 300 #Första hoppets styrka
+export var jump2_strength = 250 #Andra hoppets styrka
+#Fler styrkor för andra hopp läggs till här
 
-	if Input.is_action_pressed("jump") and is_on_floor():
-		#jumping = true
-		velocity.y = jump_speed
-		
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= run_speed
-		#walking = true
-		#$Sprite.flip_h = true
-	elif Input.is_action_pressed("move_right"):
-		velocity.x += run_speed
-		#walking = true
-		#$Sprite.flip_h = false
-	#else:
-		#walking = false
+#Variabler för karaktärens tillstånd
+var walking = false
+var jumping = false
+var falling = false
+var double_jumping = false
+#var jump_cancellation = false
+var idle = false
+
+var jumps_total = 0 #Räknare för antalet hopp gjorda
+var velocity = Vector2.ZERO #Rörelsevektorn
+
+func character_state():
+	#TRUE om karaktären går
+	walking = is_on_floor() and not is_zero_approx(velocity.x)
+	#TRUE om karaktären hoppar
+	jumping = Input.is_action_pressed("jump") and is_on_floor()
+	#TRUE om karaktären faller
+	falling = velocity.y > 0 and not is_on_floor()
+	#TRUE om karaktären dubbelhoppar (Baserat på om den faller och hoppar)
+	double_jumping = Input.is_action_just_pressed("jump") and falling
+	#TRUE om ett hopp förhindras
+	#var jump_cancellation = Input.is_action_just_released("jump") and velocity.y < 0
+	#TRUE om karaktären är stilla/idle
+	idle = is_on_floor() and is_zero_approx(velocity.x)
+	
+
+#func animation(): #AVKOMMENTERAS NÄR ANIMATIONER SKAPATS
+	#LÄGG TILL SPRITE FLIP
+	#SPELA UPP ANIMATION BASERAT PÅ TILLSTÅND
+	#Animerar karaktärens hopp
+	#if jumping or double_jumping:
+	#	(Animationsmetod)
+	#Animerar karaktärens gång
+	#if walking:
+	#	(Animationsmetod)
+	#Animerar karaktärens fall
+	#if falling:
+	#	(Animationsmetod)
+	#Animerar Karaktärens stillastånd
+	#if idle:
+	#	(Animationsmetod)
 
 func _physics_process(delta):
-	get_input()
-	velocity.y += delta * gravity
+	var walk_direction = (
+		Input.get_action_strength("move_right")
+		-Input.get_action_strength("move_left")
+	)
+	velocity.x = walk_direction * speed
+	velocity.y += gravity * delta
 	
-	#if jumping and is_on_floor():
-		#jumping = false
-	#if walking == false and velocity.x > 0:
-	#	velocity.x -= 10
-	#elif walking == false and velocity.x < 0:
-	#	veloci
-
-	velocity = move_and_slide(velocity, Vector2(0, -1))
+	character_state() #Kallar på funktionen character_state
+	
+	if jumping:
+		velocity.y = -jump_strength 
+		jumps_total += 1
+	elif double_jumping:
+		if jumps_total < jump_quantity:
+			velocity.y = -jump2_strength
+			jumps_total += 1
+	elif idle or walking:
+		jumps_total = 0
+	
+	velocity = move_and_slide(velocity, UP_DIRECTION)
+	
+	#animation()
